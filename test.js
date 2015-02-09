0f11bbf8572bd8a2da244e72651b6517de2f2dfd
Haig Papaghanian <HaigPapaghanian@quickenloans.com>
2015-02-09 18:17:03 -0500

    Initial commit.

---
+++ test.js
@@ -0,0 +1,22 @@
+var AWS = require('aws-sdk'),
+    ddb;
+
+//SET API CONFIG VALUES
+AWS.config.update({
+    credentials: new AWS.SharedIniFileCredentials({profile: 'default'}),
+    region:      'us-west-1',
+    sslEnabled:  false,
+    apiVersions: {
+        dynamodb: '2012-08-10',
+        ec2:      '2013-02-01',
+        redshift: 'latest'
+    }
+});
+
+//CREATE DYNAMODB INSTANCE
+ddb = new AWS.DynamoDB();
+
+ddb.listTables({}, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
